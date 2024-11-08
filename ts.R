788fec25ec690d382a90e91fe34da8153ba76def
[jan <jageraldez20@gmail.com>
2024-11-08 14:59:48 +0800

    Add Decomposing Time Series

--- ts.R
+++ ts.R
@@ -1,6 +1,9 @@
 # Set working directory to the folder containing your dataset
 setwd("D:\\School Workspace\\Source Codes\\R Language\\Time Series")
 
+# ==================================================================
+# SIMPLE MOVING AVERAGE
+# ==================================================================
 # Load the dataset (adjust the path as needed)
 # Install and load TTR for Simple Moving Average (SMA)
 install.packages("TTR")
@@ -19,3 +22,35 @@ lines(df$Month, moving_avg, col = "red")
 
 # Optionally, add a legend
 legend("topright", legend = c("Original", "Moving Average"), col = c("blue", "red"), lty = 1)
+# ==================================================================
+
+
+
+# ==================================================================
+# DECOMPOSING A TIME SERIES
+# ==================================================================
+
+ts_data <- ts(df$Monthly.beer.production, frequency = 12)
+decomposed_ts <- decompose(ts_data, type = "multiplicative")
+
+plot(decomposed_ts)
+
+trend_component <- decomposed_ts$trend
+seasonal_component <- decomposed_ts$seasonal
+residual_component <- decomposed_ts$random
+# ==================================================================
+
+
+
+
+
+
+# ==================================================================
+# DECOMPOSING A TIME SERIES
+# ==================================================================
+
+
+
+
+
+# ==================================================================
